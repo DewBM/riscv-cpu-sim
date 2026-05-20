ca304950f678bbd74c992042e675a7a96d82e76b
DewBM <dewmitha.mahiti@gmail.com>
2026-05-20 12:53:12 +0530

    set memory to start from 0x10000 to be compatible with QEMU

--- src/main.c
+++ src/main.c
@@ -9,33 +9,27 @@ void load_program(struct Memory *mem, const char *filename) {
         exit(EXIT_FAILURE); // Stop the program if the file can't be found/opened
     }
 
-    fread(mem->data, 1, mem->size, f);
+    fread(mem->data + 0x10000, 1, mem->size - 0x10000, f);
     fclose(f);
 }
 
 void reg_dump(struct Registers *reg) {
     for (int i=0; i<32; i++) {
-	printf("x%d: %d\n", i, reg->regs[i]);
+	printf("x%d: 0x%08x\n", i, reg->regs[i]);
     }
 }
 
 
-int main(void) {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+	fprintf(stderr, "Usage: %s <program.bin>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
     struct CPU cpu = { .halt = false };
 
     cpu_init(&cpu);
 
-/*
-    uint32_t instr[] = {0x00A00093, 0x01400113, 0x002081B3, 0x10000213, 0x00322023, 0x00000293, 0x00022283, 0x00528313, 0x00100073};
-    for (int i=0; i<9; i++){
-	mem_write(cpu.mem, i*4, instr[i], 1);
-    }
-*/
-
-//    mem_write(cpu.mem, 0, 0x00500093, 1);
-//    mem_write(cpu.mem, 4, 0x00100073, 1);
-
-    load_program(cpu.mem, "tests/bins/test3.bin");
+    load_program(cpu.mem, argv[1]);
 
     while (!cpu.halt) {
 	cpu_cycle(&cpu);
