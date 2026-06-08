0de6463f75cbd3bcef5713fe9acfe9c6ae03f250
DewBM <dewmitha.mahiti@gmail.com>
2026-06-08 18:03:42 +0530

    fix register not reading from x31 and not writing to x0 issues.

--- src/registers.c
+++ src/registers.c
@@ -6,20 +6,16 @@ void reg_init(struct Registers *reg) {
 
 void write_reg(struct Registers *reg, uint8_t rd, uint32_t rdVal, uint8_t write_enable){
     if (write_enable == 0) {
-//	fprintf(stderr, "Cannot write: write_enable is not asserted\n");
-//	exit(EXIT_FAILURE);
 	return;
     }
-    if (rd == 0) {
-	fprintf(stderr, "Cannot write to reg x0: x0 is hardwired to 0\n");
-	exit(EXIT_FAILURE);
-    }
 
-    reg->regs[rd] = rdVal;
+    if (rd > 0) {
+	reg->regs[rd] = rdVal;
+    }
 }
 
 
 void read_reg(struct Registers *reg, uint8_t rs1, uint8_t rs2, uint32_t *rs1Val, uint32_t *rs2Val) {
-    *rs1Val = rs1 < 31 ? reg->regs[rs1] : 0;
-    *rs2Val = rs2 < 32 ? reg->regs[rs2] : 0;
+    *rs1Val = rs1 <= 31 ? reg->regs[rs1] : 0;
+    *rs2Val = rs2 <= 31 ? reg->regs[rs2] : 0;
 }
